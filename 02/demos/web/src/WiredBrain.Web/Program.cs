c3d2c83b49a426d4226fe0cc427a99a1d20d0b54
Andre Lashley <andre.lashley@gmail.com>
2022-05-10 15:16:12 -0700

    Add a custom metric to the web project

--- 02/demos/web/src/WiredBrain.Web/Program.cs
+++ 02/demos/web/src/WiredBrain.Web/Program.cs
@@ -6,13 +6,17 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Prometheus;
 
 namespace WiredBrain.Web
 {
     public class Program
     {
+        private static readonly Gauge _InfoGauge = Metrics.CreateGauge("web_info", "Web app info", "dotnet_version", "assembly_name", "app_version");
+
         public static void Main(string[] args)
         {
+            _InfoGauge.Labels("3.1.7", "WiredBrain.Web", "0.1.0").Set(1);
             CreateHostBuilder(args).Build().Run();
         }
 
